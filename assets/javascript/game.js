de86789557f45f69165902c894f9f7d47ab07d17
Rodolfo <jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.comgit config --global user.email jrsotorodolfo@gmail.com>
2018-12-16 15:50:21 -0600

    Some JQuery

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+// $(document).ready(function() {
+// //Create the random number
+
+
+// //Give crystals a random value  between 1-12
+// $("#img-ruby").on("click", function() {
+//     var rubyValue = "";
+
+//     for(var i = 1; i <12; i++) {
+
+//         var random = Math.floor(Math.random() * 12 + 1);
+
+//         rubyValue = random + rubyValue;
+
+//         console.log(rubyValue);
+
+//     }
+
+//     $("#img-ruby").append("#score") + rubyValue;
+
+// })
+
+
+// })
+
+
